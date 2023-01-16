ff43d8c69919506cb1d5661bf468ef88a830dc2b
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-16 21:28:51 +0100

    idk

--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,13 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl =  process.env.SUPABASE_URL || ''
-const supabaseKey = process.env.SUPABASE_KEY || '';
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-
 export default defineEventHandler(async () => {
+  if (!process.env.SUPABASE_URL) {
+    throw new Error('Missing SUPABASE_URL environment variable');
+  }
+
+  if (!process.env.SUPABASE_KEY) {
+    throw new Error('Missing SUPABASE_KEY environment variable');
+  }
+
+  const supabase = createClient(process.env.SUPABASE_URL || '', process.env.SUPABASE_KEY || '')
+
   if (!supabase) {
-    throw new Error('Supabase not initialized');
+    throw new Error('Could not create Supabase client')
   }
 
   const { data, error } = await supabase
