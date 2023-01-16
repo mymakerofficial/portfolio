f2639c23e8fac83c07775e32367e573c475cb616
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-16 19:58:04 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-8 flex flex-col gap-8">
+    <div class="flex flex-col gap-4">
+      <h1 class="text-4xl font-extrabold">My_Maker Portfolio</h1>
+      <div class="text-sm font-medium text-neutral-600">powered by <span class="text-neutral-900 font-bold">Nuxt 3</span>, <span class="text-neutral-900 font-bold">Vite</span>, <span class="text-neutral-900 font-bold">Tailwind</span>, <span class="text-neutral-900 font-bold">Supabase</span> and <span class="text-neutral-900 font-bold">Vercel</span>.</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "My_Maker"
+})
+</script>
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>>{
+  content: [
+    `components/**/*.{vue,js,ts}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+    `Error.{js,ts,vue}`,
+    `error.{js,ts,vue}`,
+  ],
+};
