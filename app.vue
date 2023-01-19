f8089cff6c806327fae54eb8a2ebcec5cf12b28c
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-19 16:55:35 +0100

    added metadata

--- app.vue
+++ app.vue
@@ -8,6 +8,37 @@
 useHead({
   bodyAttrs: {
     class: 'dark:bg-gray-900 dark:text-gray-100',
-  }
+  },
+  htmlAttrs: {
+    lang: 'en',
+  },
+  titleTemplate: '%s - My_Maker',
+  title: 'Home',
+  meta: [
+    {
+      name: 'description',
+      content: 'Hai im My_Maker, I like making things.',
+    },
+    {
+      name: 'og:title',
+      content: 'My_Maker',
+    },
+    {
+      name: 'og:description',
+      content: 'Hai im My_Maker, I like making things.',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      name: 'twitter:title',
+      content: 'My_Maker',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Hai im My_Maker, I like making things.',
+    },
+  ]
 })
 </script>
