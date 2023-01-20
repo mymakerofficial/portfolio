73f003e71337c43814509eb77ad693f9287f2487
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-20 21:00:54 +0100

    implemented github integration for project api endpoint

--- server/api/v1/projects/[slug].ts
+++ server/api/v1/projects/[slug].ts
@@ -1,13 +1,32 @@
 import {createClient} from '@supabase/supabase-js'
+import {Octokit} from "octokit";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN,
+});
 
 const supabase = createClient(process.env.SUPABASE_URL || '', process.env.SUPABASE_KEY || '')
 
 export default defineEventHandler(async (event) => {
   const { data: projectData, error } = await supabase
     .from('projects')
-    .select('slug, displayName: display_name, summary, type, url, releaseDate: released_at_date, startedDate: started_at_date, tags ( slug, displayName: display_name ), collaborators: people ( slug, displayName: display_name, websiteUrl: website_url ), technologies ( slug, displayName: display_name, shortDisplayName: short_display_name, type: technology_type_id ( slug, displayName: display_name, shortDisplayName: short_display_name ) ), thumbnailId: thumbnail_id')
+    .select('' +
+      'slug, ' +
+      'displayName: display_name, ' +
+      'summary, ' +
+      'type, ' +
+      'url, ' +
+      'releaseDate: released_at_date, ' +
+      'startedDate: started_at_date, ' +
+      'tags ( slug, displayName: display_name ), ' +
+      'collaborators: people ( slug, displayName: display_name, websiteUrl: website_url ), ' +
+      'technologies ( slug, displayName: display_name, shortDisplayName: short_display_name, type: technology_type_id ( slug, displayName: display_name, shortDisplayName: short_display_name ) ), ' +
+      'githubRepo: github_repo, ' +
+      'keepGithubRepoSecret: keep_github_repo_secret, ' +
+      'thumbnailId: thumbnail_id'
+    )
     .eq('slug', event.context.params.slug)
-    .single()
+    .single() as any;
 
   if (!projectData || error) {
     throw new Error(error.message || 'Error fetching project');
@@ -47,6 +66,24 @@ export default defineEventHandler(async (event) => {
     }
   }
 
+  let lastCommitDate: string | null = null
+  if (projectData.githubRepo) {
+    try {
+      let githubRepo = projectData.githubRepo
+
+      // get date of last commit to master
+      const { data: githubData } = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+        owner: githubRepo.split('/')[0],
+        repo: githubRepo.split('/')[1],
+        per_page: 1
+      });
+
+      if (githubData.length > 0) {
+        lastCommitDate = githubData[0].commit.author?.date || null
+      }
+    } catch (_) {}
+  }
+
   return {
     slug: projectData.slug,
     displayName: projectData.displayName,
@@ -55,9 +92,12 @@ export default defineEventHandler(async (event) => {
     websiteUrl: projectData.url,
     releaseDate: projectData.releaseDate,
     startedDate: projectData.startedDate,
+    lastCommitDateTime: lastCommitDate,
     tags: projectData.tags,
     collaborators: projectData.collaborators,
     technologies: technologiesOut,
+    githubRepo: projectData.keepGithubRepoSecret ? null : projectData.githubRepo,
+    githubRepoUrl: projectData.keepGithubRepoSecret ? null : `https://github.com/${projectData.githubRepo}`,
     thumbnailUrl: projectData.thumbnailId ? `/api/v1/projects/${projectData.slug}/thumbnail` : null,
   };
 });
