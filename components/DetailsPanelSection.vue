1698b575bf25f07846c679314a165a0fbfd94ab9
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-21 11:58:15 +0100

    more padding, more better

--- components/DetailsPanelSection.vue
+++ components/DetailsPanelSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-8 flex flex-col gap-4 border-b-2 last:border-b-0 border-white dark:border-gray-900">
+  <div class="p-8 md:p-12 flex flex-col gap-4 border-b-2 last:border-b-0 border-white dark:border-gray-900">
     <h1 class="text-xl font-bold text-gray-800 dark:text-gray-100">{{ title }}</h1>
     <slot />
   </div>
