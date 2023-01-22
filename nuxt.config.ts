1f6acd42ed14347e5688d27b75b83798cb63bb86
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-22 19:16:07 +0100

    added route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  routeRules: {
+    '/_nuxt/**': { headers: { 'cache-control': 's-maxage=0' } },
+    '/admin/**': { ssr: false },
+    '/api/v1/**': { cors: true },
+  }
 })
