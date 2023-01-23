66fc874997ec80807dc71e03f1467318c29f318c
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-23 22:30:41 +0100

    implemented icons

--- components/NavBarButton.vue
+++ components/NavBarButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLink class="h-full px-12 flex items-center font-medium">
+  <NuxtLink class="h-full px-12 flex flex-row gap-2 items-center font-medium">
     <slot />
   </NuxtLink>
 </template>
--- components/NavBarWrapper.vue
+++ components/NavBarWrapper.vue
@@ -1,20 +1,26 @@
 <template>
   <NavBar :active-index="activeIndex">
     <NavBarButton v-for="option in options" :key="option.label" :href="option.href">
-      {{ option.label }}
+      <SvgIcon type="mdi" :path="option.iconPath" v-if="option.iconPath" />{{ option.label }}
     </NavBarButton>
   </NavBar>
 </template>
 
 <script lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon';
 import {PropType} from "@vue/runtime-core";
 
 export interface NavBarOption {
   href: string;
   label: string;
+  iconPath?: string;
 }
 
 export default defineNuxtComponent({
+  components: {
+    SvgIcon
+  },
+
   props: {
     options: {
       type: Array as PropType<NavBarOption[]>,
