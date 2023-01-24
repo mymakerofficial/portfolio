64e4dd2bb59223d68febea0e04bdc277f3ace0ba
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-24 22:46:32 +0100

    added query to project page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,11 +1,6 @@
 <template>
-  <Container>
-    <div class="mt-12 mb-40 md:mt-32 md:mb-48 flex flex-col gap-12">
-      <div class="flex flex-row gap-2">
-        <Button @click="groupByProperty = 'date';groupByValue = 'year';this.fetchGroups();">Year</Button>
-        <Button @click="groupByProperty = 'technology-type';groupByValue = 'programming-language';this.fetchGroups();">Language</Button>
-        <Button @click="groupByProperty = 'technology-type';groupByValue = 'frontend-framework';this.fetchGroups();">Framework</Button>
-      </div>
+  <Container class="2xl:w-1/2">
+    <div class="mt-12 mb-40 md:mt-44 md:mb-48 flex flex-col gap-12">
       <ProjectsList :groups="groups"/>
     </div>
   </Container>
@@ -14,8 +9,26 @@
 <script lang="ts">
 export default defineNuxtComponent({
   async setup () {
-    const groupByProperty = 'date';
-    const groupByValue = 'year';
+    let groupByProperty = 'date';
+    let groupByValue = 'year';
+
+    if (useRoute().query.group_by) {
+      const groupBy = useRoute().query.group_by as string;
+
+      if (groupBy === 'date') {
+        groupByProperty = 'date';
+        groupByValue = 'year';
+      } else if (groupBy === 'language') {
+        groupByProperty = 'technology-type';
+        groupByValue = 'programming-language';
+      } else if (groupBy === 'framework') {
+        groupByProperty = 'technology-type';
+        groupByValue = 'frontend-framework';
+      }
+    } else if (useRoute().query.group_by_property && useRoute().query.group_by_value) {
+      groupByProperty = useRoute().query.group_by_property as string;
+      groupByValue = useRoute().query.group_by_value as string;
+    }
 
     // fetch projects from api
     const { data: groups } = await useFetch(`/api/v1/grouped_projects?group_by_property=${groupByProperty}&group_by_value=${groupByValue}`);
