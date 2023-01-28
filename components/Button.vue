ab7465e3701e6909b5820e231a084d06d3c890b5
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-01-28 19:19:57 +0100

    fancy button

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,9 @@
 <template>
-  <span class="h-12 px-10 inline-flex justify-center items-center text-md font-medium bg-gray-100 text-gray-900 dark:bg-gray-800 dark:text-gray-100 rounded-xl">
-    <slot />
+  <span class="group/button inline-block rounded-full overflow-hidden bg-gray-100 dark:bg-gray-800 shadow-xl shadow-transparent hover:shadow-gray-500/10 hover:dark:shadow-gray-600/10 duration-500 ease-in-out">
+    <span class="w-full h-full absolute bg-gradient-to-br from-gray-50 via-gray-100 to-gray-50 dark:from-gray-800 dark:via-gray-700 dark:to-gray-800 group-hover/button:rotate-180 group-hover/button:brightness-75 dark:group-hover/button:brightness-200 transition duration-500 ease-in-out" />
+    <span class="w-full h-full absolute bg-gray-500 dark:bg-gray-100 opacity-0 group-hover/button:opacity-20 duration-500 ease-in-out" />
+    <span class="h-12 m-0.5 px-10 inline-flex justify-center items-center text-md font-medium text-gray-900 dark:text-gray-100 bg-gray-50 dark:bg-gray-800 rounded-full">
+      <slot />
+    </span>
   </span>
 </template>
