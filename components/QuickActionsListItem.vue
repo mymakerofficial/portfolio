3b747696a71de0defc56bff197dbb645cc463d72
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-02-03 21:04:24 +0100

    focused highlight

--- components/QuickActionsListItem.vue
+++ components/QuickActionsListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="item.action" class="mx-1.5 p-3 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg transition-colors ease-in-out duration-200">
+  <button @click="item.action" class="mx-1.5 p-3 hover:bg-gray-100 dark:hover:bg-gray-700 focus:bg-gray-100 dark:focus:bg-gray-700 focus:outline-0 rounded-lg transition-colors ease-in-out duration-200">
     <span class="flex flex-col md:flex-row gap-2 md:items-center">
       <span class="text-sm font-medium text-gray-600 dark:text-gray-200 truncate">{{ item.displayName }}</span>
     </span>
