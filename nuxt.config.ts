4791e846d6bf673c8c53222ea4106b4e36324e1c
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-03-07 17:53:41 +0100

    added analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
   routeRules: {
     '/_nuxt/**': { headers: { 'cache-control': 's-maxage=0' } },
     '/admin/**': { ssr: false },
