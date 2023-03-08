597d465845e690c7db7deaee0abc24483eb47b9a
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-03-08 21:07:17 +0100

    trust me bro

--- server/routes/cdn/[...path].ts
+++ server/routes/cdn/[...path].ts
@@ -2,8 +2,8 @@ import {h3SendMediaFromUrl} from "~/lib/h3mediaProxy";
 
 export default defineEventHandler(async (event) => {
   if (process.env.STORAGE_PROVIDER === 'supabase') {
-    return h3SendMediaFromUrl(event, `${process.env.SUPABASE_URL}/storage/v1/object/public/${process.env.SUPABASE_MEDIA_BUCKET_NAME}/${event.context.params.path}`);
+    return h3SendMediaFromUrl(event, `${process.env.SUPABASE_URL}/storage/v1/object/public/${process.env.SUPABASE_MEDIA_BUCKET_NAME}/${event.context.params!.path}`);
   } else if (process.env.STORAGE_PROVIDER === 'firestore') {
-    return h3SendMediaFromUrl(event, `${process.env.STORAGE_URL}/${encodeURIComponent(event.context.params.path.replace(/^\//, ''))}?alt=media`);
+    return h3SendMediaFromUrl(event, `${process.env.STORAGE_URL}/${encodeURIComponent(event.context.params!.path.replace(/^\//, ''))}?alt=media`);
   }
 })
