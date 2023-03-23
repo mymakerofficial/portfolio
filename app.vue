f046a2c8ca2b4dea0cdfd41c5c83327db13208ae
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-03-23 22:39:37 +0100

    added a dev version warning

--- app.vue
+++ app.vue
@@ -3,9 +3,12 @@
     <ClientOnly>
       <QuickActionModal v-model:active="quickActionModalActive" />
     </ClientOnly>
-    <!--<NavBarWrapper :options="navBarOptions" :active="navBarActive" />-->
+    <NavBarWrapper :options="navBarOptions" :active="navBarActive" />
     <NuxtLoadingIndicator color="#f6f8fa" :height="2" />
     <NuxtPage />
+    <ClientOnly>
+      <DevVersionWarningMessage />
+    </ClientOnly>
   </div>
 </template>
 
@@ -14,7 +17,8 @@ import {NavBarOption} from "~/components/navbar/NavBarWrapper.vue";
 import {get, set, useMagicKeys, whenever} from "@vueuse/core";
 import {computed, ref} from "vue";
 import QuickActionModal from "~/components/quickactions/QuickActionModal.vue";
-//import NavBarWrapper from "~/components/navbar/NavBarWrapper.vue";
+import DevVersionWarningMessage from "~/components/goodies/DevVersionWarningMessage.vue";
+import NavBarWrapper from "~/components/navbar/NavBarWrapper.vue";
 
 const navBarOptions: NavBarOption[] = [
   { href: '/', label: 'home' },
