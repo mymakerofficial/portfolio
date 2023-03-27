b0396c6f866e18848389f52841643d6768e7d1d1
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-03-27 20:33:56 +0200

    a little bit of margin

--- components/projectpage/ProjectPageHero.vue
+++ components/projectpage/ProjectPageHero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-16">
-    <div class="flex flex-col gap-4">
+    <div class="sm:px-8 flex flex-col gap-4">
       <h1 class="text-2xl md:text-4xl xl:text-5xl font-extrabold text-gray-800 dark:text-gray-100">{{ project.displayName }}</h1>
       <p class="font-medium text-gray-600 dark:text-gray-300">{{ project.summary }}</p>
     </div>
@@ -8,7 +8,7 @@
       <img :src="project.thumbnailUrl" :alt="project.displayName" class="absolute w-full h-full z-10" />
       <div class="w-full h-full bg-gray-600/20 dark:bg-gray-100/20 animate-pulse" />
     </div>
-    <div>
+    <div class="sm:px-8">
       <div v-if="project.websiteUrl">
         <FlatButton :href="project.websiteUrl" target="_blank"><SvgIcon type="mdi" :path="mdiWeb" class="h-5" />Visit Website</FlatButton>
       </div>
