ccf92c54895e2211619b1f8029eccac5b0a1fd37
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-03-30 15:45:46 +0200

    made details cards

--- components/detailspanel/DetailsPanelCard.vue
+++ components/detailspanel/DetailsPanelCard.vue
@@ -1,6 +1,10 @@
 <template>
-  <component :is="$attrs.href ? 'a' : 'div'" class="rounded-xl bg-gray-50 dark:bg-gray-800 p-8 md:p-12 flex flex-col gap-4">
+  <Card is="div" hoverable class="p-8 md:p-12 flex flex-col gap-4">
     <h1 class="text-xl font-bold text-gray-800 dark:text-gray-100 flex gap-3 items-center" v-if="$slots.title"><slot name="title" /></h1>
     <slot />
-  </component>
+  </Card>
 </template>
+
+<script setup lang="ts">
+import Card from "~/components/generics/Card.vue";
+</script>
