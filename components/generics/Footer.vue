a7bb38876ae0d1ff5624189df2a46c8a7a8d74b5
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-03-30 15:28:18 +0200

    improved footer

--- components/generics/Footer.vue
+++ components/generics/Footer.vue
@@ -1,12 +1,11 @@
 <template>
   <footer class="mx-auto px-8">
-    <div class="text-md font-medium text-gray-300 dark:text-gray-600 fill-gray-300 dark:fill-gray-600">
-      Proudly build with <NuxtIcon class="h-4 inline" /> Nuxt, <TailwindIcon class="h-2.5 inline" /> Tailwind and other awesome technologies.
+    <div class="text-md text-gray-300 dark:text-gray-600 fill-gray-300 dark:fill-gray-600">
+      Proudly build with <NuxtIcon class="h-4 inline" /> Nuxt and other awesome technologies.
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
 import NuxtIcon from "~/components/icons/NuxtIcon.vue";
-import TailwindIcon from "~/components/icons/TailwindIcon.vue";
 </script>
