8762b3fe6270bd4c16aa0d75a9098443d8dbc2d3
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-04-02 20:39:23 +0200

    removed new sticker for now

--- components/home/ProjectCard.vue
+++ components/home/ProjectCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <DraggableSticker v-if="stickers.isJs" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="40">
+    <DraggableSticker v-if="stickers.isJs" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
       <JsSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isTs" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="40">
+    <DraggableSticker v-if="stickers.isTs" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
       <TsSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
     <DraggableSticker v-if="stickers.isVue" class="absolute right-8 md:-right-4 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
@@ -14,9 +14,6 @@
     </DraggableSticker>
     <DraggableSticker v-if="stickers.isChess" class="absolute right-[5%] -top-2 z-10" :rotation="-14">
       <ChessSticker class="h-16 md:h-20 pointer-events-none" />
-    </DraggableSticker>
-    <DraggableSticker v-if="stickers.isNew" class="absolute right-[10%] -top-3 z-10" :rotation="12">
-      <NewSticker class="h-12 pointer-events-none" />
     </DraggableSticker>
     <NuxtLink :to="project.htmlUrl" class="rounded-2xl">
       <Card :hoverable="true" class="flex flex-col gap-6 p-8">
@@ -41,7 +38,6 @@ import {CompactProjectInfo} from "~/lib/projects";
 import Card from "~/components/generics/Card.vue";
 import {computed, reactive} from "vue";
 import dayjs from "dayjs";
-import NewSticker from "~/components/stickers/NewSticker.vue";
 import DraggableSticker from "~/components/stickers/DraggableSticker.vue";
 import ChessSticker from "~/components/stickers/ChessSticker.vue";
 import JsSticker from "~/components/stickers/JsSticker.vue";
@@ -57,11 +53,6 @@ const props = defineProps<{
 const dateFormatted = computed(() => dayjs(props.project.date).format('MMMM, YYYY'))
 
 const stickers = reactive({
-  isNew: computed(() => {
-    const date = dayjs(props.project.date);
-    const now = dayjs();
-    return now.diff(date, 'month') < 3;
-  }),
   isChess: computed(() => props.project.tags.includes('chess')),
   isJs: computed(() => props.project.technologies.includes('js') && !props.project.technologies.includes('ts')),
   isTs: computed(() => props.project.technologies.includes('ts')),
