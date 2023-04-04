ceecde15504a3df795749df8d3d77b17066d62f0
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-04-04 18:59:59 +0200

    fixed button opacity

--- components/forms/FlatButton.vue
+++ components/forms/FlatButton.vue
@@ -3,7 +3,7 @@
     class="relative inline-block rounded-2xl overflow-hidden bg-gray-100 dark:bg-gray-800 duration-500 ease-in-out"
   >
     <ClientOnly>
-      <SpotlightEffectElement />
+      <SpotlightEffectElement class="opacity-10" />
     </ClientOnly>
     <span class="h-12 px-6 inline-flex justify-center items-center gap-2 text-md font-medium text-gray-900 dark:text-gray-100">
       <slot />
