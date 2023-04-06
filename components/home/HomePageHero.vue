111e2a975dccd2fbb349d20250055dca9aa783d8
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-04-06 14:57:08 +0200

    added github link

--- components/home/HomePageHero.vue
+++ components/home/HomePageHero.vue
@@ -1,8 +1,8 @@
 <template>
   <HomePageHeroBackground />
   <Container class="2xl:w-5/12">
-    <div class="flex flex justify-center">
-      <div class="mx-8 md:mx-12">
+    <div class="flex flex-col gap-8 justify-center mx-8 md:mx-12">
+      <div>
         <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100 leading-loose">Hai I'm My_Maker.</h1>
         <div class="text-lg font-medium text-gray-900 dark:text-gray-100 leading-loose tracking-wide">
           Making mostly silly websites and games online.
@@ -10,10 +10,19 @@
           Below you'll find all my projects and some other random stuff about me.
         </div>
       </div>
+      <div class="text-gray-600 dark:text-gray-300">
+        <GenericButton class="inline-flex gap-2 font-medium" :href="GitHubUrl"><SvgIcon type="mdi" :path="mdiGithub" />GitHub</GenericButton>
+      </div>
     </div>
   </Container>
 </template>
 
 <script setup lang="ts">
 import Container from "~/components/generics/Container.vue";
+import GenericButton from "~/components/forms/GenericButton.vue";
+//@ts-ignore
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiGithub } from '@mdi/js';
+
+const GitHubUrl = import.meta.env.VITE_GITHUB_URL;
 </script>
