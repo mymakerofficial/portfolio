fba812a1c95ae45d752ea35c6d61ef133c4dc1f2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-05-07 14:28:43 +0200

    updated footer

--- components/generics/Footer.vue
+++ components/generics/Footer.vue
@@ -1,17 +1,24 @@
 <template>
   <footer class="mx-auto px-8 my-24">
-    <div class="flex flex-col gap-4 text-center text-md text-gray-300 dark:text-gray-600 fill-gray-300 dark:fill-gray-600">
-      <p>
-        Proudly build with <NuxtIcon class="h-4 inline" /> Nuxt and other awesome technologies.
-      </p>
-      <div class="flex gap-4 justify-center underline">
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/projects">Projects</NuxtLink>
-        <NuxtLink to="/legal">Privacy Policy</NuxtLink>
+    <div class="flex flex-col gap-8 text-center text-md text-gray-300 dark:text-gray-600 fill-gray-300 dark:fill-gray-600">
+      <div class="flex flex-col gap-8">
+        <div class="flex flex-row gap-8 justify-center items-center">
+          <QuickactionsOpenQuickActionModalButton />
+        </div>
+        <div class="flex gap-4 justify-center underline">
+          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="/projects">Projects</NuxtLink>
+          <NuxtLink to="/legal">Privacy Policy</NuxtLink>
+        </div>
       </div>
-      <p>
-        © 2023 My_Maker. All rights reserved.
-      </p>
+      <div class="flex flex-col gap-2">
+        <p>
+          Proudly build with <NuxtIcon class="h-4 inline" /> Nuxt and other awesome technologies.
+        </p>
+        <p>
+          © 2023 My_Maker. All rights reserved. All trademarks are property of their respective owners.
+        </p>
+      </div>
     </div>
   </footer>
 </template>
--- components/quickactions/OpenQuickActionModalButton.vue
+++ components/quickactions/OpenQuickActionModalButton.vue
@@ -1,10 +1,10 @@
 <template>
-  <button @click="openModal" class="select-none h-9 px-2 inline-flex flex-row gap-3 items-center font-medium text-gray-300 dark:text-gray-700 border-2 border-gray-100 dark:border-gray-700 contrast-more:text-gray-500 contrast-more:dark:text-gray-400 contrast-more:border-gray-500 contrast-more:dark:border-gray-400 rounded-lg">
+  <button @click="openModal" class="select-none h-9 px-2 inline-flex flex-row gap-3 items-center font-medium text-gray-300 dark:text-gray-700 border border-gray-100 dark:border-gray-700 contrast-more:text-gray-500 contrast-more:dark:text-gray-400 contrast-more:border-gray-500 contrast-more:dark:border-gray-400 rounded-lg">
     <span class="flex gap-1 items-center">
       <SvgIcon type="mdi" :path="mdiMagnify" size="20" />
       <span class="font-medium">Search</span>
     </span>
-    <span class="font-medium">Ctrl K</span>
+    <span class="font-medium">Ctrl+K</span>
   </button>
 </template>
 
