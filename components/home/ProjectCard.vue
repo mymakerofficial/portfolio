48ef8fba0399b223c2047feb25bf996f30d11178
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-06-21 19:35:03 +0200

    implemented games page

--- components/home/ProjectCard.vue
+++ components/home/ProjectCard.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
-    <DraggableSticker v-if="stickers.isJs" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
+    <DraggableSticker v-if="stickers.isJs && showStickers" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
       <JsSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isTs" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
+    <DraggableSticker v-if="stickers.isTs && showStickers" class="absolute right-[10%] top-10 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
       <TsSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isVue" class="absolute right-8 md:-right-4 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
+    <DraggableSticker v-if="stickers.isVue && showStickers" class="absolute right-8 md:-right-4 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
       <VueSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isNuxt" class="absolute right-8 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
+    <DraggableSticker v-if="stickers.isNuxt && showStickers" class="absolute right-8 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
       <NuxtSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isChess" class="absolute right-[5%] -top-2 z-10" :rotation="-14">
+    <DraggableSticker v-if="stickers.isChess && showStickers" class="absolute right-[5%] -top-2 z-10" :rotation="-14">
       <ChessSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isGame" class="absolute right-[12%] md:-right-[3%] top-12 md:top-6 z-10" :rotation="0" :rotation-randomize="23">
+    <DraggableSticker v-if="stickers.isGame && showStickers" class="absolute right-[12%] md:-right-[3%] top-12 md:top-6 z-10" :rotation="0" :rotation-randomize="23">
       <JoystickSticker class="h-20 md:h-28 pointer-events-none" />
     </DraggableSticker>
-    <DraggableSticker v-if="stickers.isCs" class="absolute right-[6%] top-20 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
+    <DraggableSticker v-if="stickers.isCs && showStickers" class="absolute right-[6%] top-20 z-10" :rotation-randomize="30" :x-randomize="30" :y-randomize="30">
       <CsSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
     <NuxtLink :to="project.htmlUrl" class="rounded-2xl">
       <Card :hoverable="true" class="flex flex-col gap-6 p-8">
-        <div v-if="showThumbnail" class="w-full aspect-video rounded-lg overflow-hidden">
+        <div v-if="showThumbnail && project.thumbnailUrl" class="w-full aspect-video rounded-lg overflow-hidden">
           <img :src="project.thumbnailUrl" :alt="project.displayName" class="absolute w-full h-full z-10" />
           <div class="w-full h-full bg-gray-600/20 dark:bg-gray-100/20 animate-pulse" />
         </div>
@@ -56,17 +56,18 @@ import CsSticker from "~/components/stickers/CsSticker.vue";
 const props = defineProps<{
   project: CompactProjectInfo
   showThumbnail?: boolean;
+  showStickers?: boolean;
 }>();
 
-const dateFormatted = useDateFormat(props.project.date, 'MMMM, YYYY')
+const dateFormatted = useDateFormat(props.project?.date, 'MMMM, YYYY')
 
 const stickers = reactive({
-  isChess: computed(() => props.project.tags.includes('chess')),
-  isGame: computed(() => props.project.type === "game" && !props.project.tags.includes('chess')),
-  isJs: computed(() => props.project.technologies.includes('js') && !props.project.technologies.includes('ts')),
-  isTs: computed(() => props.project.technologies.includes('ts')),
-  isCs: computed(() => props.project.technologies.includes('cs')),
-  isVue: computed(() => (props.project.technologies.includes('vue-2') || props.project.technologies.includes('vue-3')) && !props.project.technologies.includes('nuxt-3')),
-  isNuxt: computed(() => props.project.technologies.includes('nuxt-3')),
+  isChess: computed(() => props.project?.tags.includes('chess')),
+  isGame: computed(() => props.project.type === "game" && !props.project?.tags.includes('chess')),
+  isJs: computed(() => props.project?.technologies.includes('js') && !props.project?.technologies.includes('ts')),
+  isTs: computed(() => props.project?.technologies.includes('ts')),
+  isCs: computed(() => props.project?.technologies.includes('cs')),
+  isVue: computed(() => (props.project?.technologies.includes('vue-2') || props.project?.technologies.includes('vue-3')) && !props.project?.technologies.includes('nuxt-3')),
+  isNuxt: computed(() => props.project?.technologies.includes('nuxt-3')),
 });
 </script>
---
+++ pages/games/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex flex-col gap-12 md:gap-28 my-12 md:my-36">
+    <section>
+      <Container class="2xl:w-5/12">
+        <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100 leading-loose">Games</h1>
+        <div class="text-lg font-medium text-gray-900 dark:text-gray-100 leading-loose tracking-wide">
+          I worked on some games. You can play them here.
+        </div>
+      </Container>
+    </section>
+    <main>
+      <Container class="2xl:w-5/12">
+        <div class="flex flex-col gap-6">
+          <ProjectCard v-for="item in games" :key="item.key" :project="item" show-thumbnail />
+        </div>
+      </Container>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Container from "~/components/generics/Container.vue";
+import {defineOgImageScreenshot, useProjectsList} from "#imports";
+import {useSeoMeta} from "unhead";
+import {get, set} from "@vueuse/core";
+import {CompactProjectInfo} from "~/lib/projects";
+import {ref} from "vue";
+import ProjectCard from "~/components/home/ProjectCard.vue";
+
+useSeoMeta({
+  title: "Games by My_Maker",
+  description:  "Hai Im My_Maker. I make games and you can play them here.",
+});
+
+defineOgImageScreenshot({
+  width: 1280,
+  height: 720,
+  colorScheme: 'dark',
+  mask: '.fun-card, .toast, nav',
+});
+
+// fetch projects
+const projectsResponse = await useProjectsList({
+  featuredFist: true,
+  type: 'game',
+});
+
+const games = ref<CompactProjectInfo[]>();
+
+if (get(projectsResponse)?.data !== null) {
+  set(games, get(projectsResponse).data);
+}
+</script>
