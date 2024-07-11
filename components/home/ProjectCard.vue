c50aae471aafc0300ae11fcf4dc0ebe92eb57fb5
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-11 20:30:07 +0200

    add react sticker

--- components/home/ProjectCard.vue
+++ components/home/ProjectCard.vue
@@ -12,6 +12,9 @@
     <DraggableSticker v-if="stickers.isSvelte && showStickers" class="absolute right-8 md:-right-4 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
       <SvelteSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
+    <DraggableSticker v-if="stickers.isReact && showStickers" class="absolute right-8 md:-right-4 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
+      <ReactSticker class="h-16 md:h-20 pointer-events-none" />
+    </DraggableSticker>
     <DraggableSticker v-if="stickers.isNuxt && showStickers" class="absolute right-8 bottom-0 z-10" :rotation="-10" :rotation-randomize="4" :x-randomize="30" :y-randomize="20">
       <NuxtSticker class="h-16 md:h-20 pointer-events-none" />
     </DraggableSticker>
@@ -27,7 +30,7 @@
     <NuxtLink :to="project.htmlUrl" class="rounded-2xl">
       <Card :hoverable="true" class="flex flex-col gap-6 p-8">
         <div v-if="showThumbnail && project.thumbnailUrl" class="w-full aspect-video rounded-lg overflow-hidden">
-          <img :src="project.thumbnailUrl" :alt="project.displayName" class="absolute w-full h-full z-10" />
+          <img :src="project.thumbnailUrl" :alt="project.displayName" class="absolute w-full h-full z-[5]" />
           <div class="w-full h-full bg-gray-600/20 dark:bg-gray-100/20 animate-pulse" />
         </div>
         <div class="flex flex-col gap-3">
@@ -56,6 +59,7 @@ import JoystickSticker from "~/components/stickers/JoystickSticker.vue";
 import {useDateFormat} from "@vueuse/core";
 import CsSticker from "~/components/stickers/CsSticker.vue";
 import SvelteSticker from "~/components/stickers/SvelteSticker.vue";
+import ReactSticker from "~/components/stickers/ReactSticker.vue";
 
 const props = defineProps<{
   project: CompactProjectInfo
@@ -73,6 +77,7 @@ const stickers = reactive({
   isCs: computed(() => props.project?.technologies.includes('cs')),
   isVue: computed(() => (props.project?.technologies.includes('vue-2') || props.project?.technologies.includes('vue-3')) && !props.project?.technologies.includes('nuxt-3')),
   isSvelte: computed(() => props.project?.technologies.includes('svelte')),
+  isReact: computed(() => props.project?.technologies.includes('react')),
   isNuxt: computed(() => props.project?.technologies.includes('nuxt-3')),
 });
 </script>
